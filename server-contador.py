4ae64371e17d2e96f18877b8be6ef801a17d0756
juan <juanurenagarciagasco@gmail.com>
2018-03-05 23:27:05 +0100

    Opcion con varios contadores

--- server-contador.py
+++ server-contador.py
@@ -12,7 +12,7 @@ import contador
 import socket
 import random 
 
-dic_Contador{}
+dic_Contador={}
 
 #Creamos TCP socket
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,7 +24,7 @@ name=socket.gethostname()
 mySocket.bind((name, 1234))
 
 #Fijamos número máximo de usuarios a 5
-mySocket.listen(5)
+mySocket.listen(100)
 
 #Estamos listos para recibir solicitudes
 
@@ -37,13 +37,31 @@ while True:
     (recvSocket, address) = mySocket.accept()
     print('Request received:')
     request=str(recvSocket.recv(2048), 'utf-8')
+    print(request)
     resource=request.split()[1]
-    print(resource)
-   
-    if resource=='/contador':      
-        URL = "/contador/"+str(random.randrange(999999999))
-        answer=("HTTP/1.1 200 OK\r\n\r\n"+"<html><body><h1>Contador Online</h1> <a href="+URL+"> Este es tu contador</a> </body></html>" + "\r\n")
-    else:
+    #print(resource)
+ 
+    try:  
+        if resource=='/contador':
+            notunique=True
+            while notunique:
+                counter=str(random.randrange(999999999))    
+                if (counter in dic_Contador)==False:     
+                    URL = "/contador/"+counter
+                    dic_Contador[counter]=0
+                    notunique=False
+        
+            answer=("HTTP/1.1 200 OK\r\n\r\n"+"<html><body><h1>Contador Online</h1> <a href="+URL+"> Este es tu contador</a> </body></html>" + "\r\n")
+        else:
+            print(resource)
+            _,resource, code=resource.split('/')
+            status=contador.contador(dic_Contador[code])
+            dic_Contador[code]=status
+            answer=("HTTP/1.1 200 OK\r\n\r\n"+"<html><body><h1>Contador Online</h1> El estado actual de su contador es "+str(status)+" </body></html>" + "\r\n")
+      
+      
+    except ValueError:
+      
         answer=("HTTP/1.1 404 NOT FOUND\r\n\r\n" +"<html><body><h1>Contador Online</h1>" +'Recurso no encontrado' + "</body></html>" +"\r\n")
     recvSocket.send(bytes(answer, 'utf-8'))
                        
